b230dcca4a565e4f9b4959634fd2fad7cb1fce17
Angel S <freetison@gmail.com>
2023-02-06 23:17:15 -0300

    Added game list array as demo

---
+++ src/app/features/board/gamesList.js
@@ -0,0 +1,36 @@
+const gamesList = {
+  games: [
+    {
+      id: 1,
+      stadium: "Estadio Azteca",
+      home: "Mexico",
+      away: "Canada",
+    },
+    {
+      id: 2,
+      stadium: "Bernabéu",
+      home: "Spain",
+      away: "Brazil",
+    },
+    {
+      id: 3,
+      stadium: "Saint-Denis",
+      home: "Germany",
+      away: "France",
+    },
+    {
+      id: 4,
+      stadium: "San Siro​​",
+      home: "Uruguay",
+      away: "Italy",
+    },
+    {
+      id: 5,
+      stadium: "La Bombonera",
+      home: "Argentina",
+      away: "Australia",
+    },
+  ],
+};
+
+export default gamesList.games;
