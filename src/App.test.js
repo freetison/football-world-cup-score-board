28d2bd7420fb4795fa2d4c0d9d194d237b44efc9
Angel S <freetison@gmail.com>
2023-02-06 23:03:09 -0300

    Fail render Scoreboard test

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,19 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("<App />", () => {
-  test("it should render header", () => {
+  test("it should render Header", () => {
     render(<App />);
 
     const header = screen.getByTestId("Header");
 
     expect(header).toBeInTheDocument();
+  });
+
+  test("it should render Scoreboard", () => {
+    render(<App />);
+
+    const scoreboard = screen.getByTestId("Scoreboard");
+
+    expect(scoreboard).toBeInTheDocument();
   });
 });
